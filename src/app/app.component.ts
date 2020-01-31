e5e4026e113cda834cd5ea07d73cbdc348396664
Jeremy Lucas <jplucas14@gmail.com>
2020-01-30 17:45:12 -0700

    fixing some router issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { WorkoutsComponent } from "./workouts/workouts.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'velo-coach-app';
-  
+  title = "velo-coach-app";
+
+  constructor(private workoutsComponent: WorkoutsComponent) {}
 }
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy, Output } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: "app-sidenav",
   templateUrl: "./sidenav.component.html",
   styleUrls: ["./sidenav.component.scss"]
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit, OnDestroy {
   panelOpenState: boolean = false;
+  subscription;
 
-  closePanel() {
-    this.panelOpenState = !this.panelOpenState;
+  constructor(private router: Router) {}
+
+  ngOnInit() {}
+
+  routerChange() {
+    if (this.router.url.includes("trainingplans")) {
+      console.log("training");
+    } else if (this.router.url.includes("workouts")) {
+      console.log("workouts");
+    }
+    return;
   }
+
+  ngOnDestroy() {}
 }
