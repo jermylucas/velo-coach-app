d62e576f6500a645d3b66b6b36b542b77bb94b41
Jeremy Lucas <jplucas14@gmail.com>
2020-02-13 13:46:13 -0700

    removed html100% and rem units across all stylesheets

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-page-not-found",
   template: `
-    <h1 style="font-size: 4.8rem;">404 Error: Page not found"</h1>
+    <h1 style="font-size: 48px;">404 Error: Page not found"</h1>
   `
 })
 export class PageNotFoundComponent {}
