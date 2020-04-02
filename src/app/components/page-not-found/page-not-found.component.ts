dbe392c18072d8f8d5c4769b862650d7056ec9f7
Jeremy Lucas <jplucas14@gmail.com>
2020-04-02 17:08:16 -0600

    Added routerLink to page not found

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -3,7 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-page-not-found",
   template: `
-    <h1 style="font-size: 48px;">404 Error: Page not found"</h1>
+    <div style=" margin-left: 20px">
+      <h1 style="font-size: 35px;">
+        404 Error: Page not found"
+      </h1>
+      <a routerLink="/dashboard">
+        Click here to go to dashboard or to sign in</a
+      >
+    </div>
   `
 })
 export class PageNotFoundComponent {}
