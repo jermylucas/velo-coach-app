6796d632166cbd3130bcf8aa08a6d1d40ac17712
Jeremy Lucas <jplucas14@gmail.com>
2020-04-03 17:31:43 -0600

    Set up new service to view and edit user profile in account

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.19.
 
 This project is a dashboard for coaches to make and edit training plans and workouts.
+
+Each user can store his or her own workouts with the appropriate details in the app. This app is currently in progress. Please contact me with any questions...
--- src/app/features/account/account.component.ts
+++ src/app/features/account/account.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProfileService } from "src/app/core/services/profile.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.scss"],
 })
 export class AccountComponent implements OnInit {
+  profileSub;
+  activeUser;
+  editMode: boolean = false;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit() {
+    this.profileSub = this.profileService.getCurrentUser().subscribe((res) => {
+      this.activeUser = res;
+    });
   }
 
+  changeMode() {
+    this.editMode = !this.editMode;
+  }
 }
