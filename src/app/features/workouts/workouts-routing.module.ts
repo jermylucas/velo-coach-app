8d88c875f4c1eaf7adfedd58ec83d428b1512a36
Jeremy Lucas <jplucas@overlandwest.com>
2021-07-04 15:42:18 -0600

    Fixed edit page routing issues

--- src/app/features/workouts/workouts-routing.module.ts
+++ src/app/features/workouts/workouts-routing.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WorkoutsComponent } from './workouts.component';
 import { WorkoutDetailComponent } from '../workouts/workout-detail/workout-detail.component';
+import { WorkoutEditComponent } from './workout-edit/workout-edit.component';
 
 const routes: Routes = [
   { path: '', component: WorkoutsComponent },
-  // { path: 'new', component: WorkoutEditComponent },
+  { path: 'new', component: WorkoutEditComponent },
   {
     path: ':id',
     component: WorkoutDetailComponent,
   },
-  // {
-  //   path: ':id/edit',
-  //   component: WorkoutEditComponent,
-  // },
+  {
+    path: ':id/edit',
+    component: WorkoutEditComponent,
+  },
 ];
 
 @NgModule({
